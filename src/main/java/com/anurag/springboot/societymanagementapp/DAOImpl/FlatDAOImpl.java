caba8b356868bb94e1a83913c809066a7f528ad7
Anurag Kulkarni <kanurag0801@gmail.com>
2022-06-02 00:00:04 +0530

    Updated table name

--- src/main/java/com/anurag/springboot/societymanagementapp/DAOImpl/FlatDAOImpl.java
+++ src/main/java/com/anurag/springboot/societymanagementapp/DAOImpl/FlatDAOImpl.java
@@ -21,28 +21,28 @@ public class FlatDAOImpl implements FlatDAO {
 
     @Override
     public int create(Flat flat) {
-        String sql = "INSERT INTO flats (fl_no, rooms, balcony, area) values (?, ?, ?, ?)";
+        String sql = "INSERT INTO Flats (fl_no, rooms, balcony, area) values (?, ?, ?, ?)";
         int create = jdbcTemplate.update(sql, flat.getFl_no(), flat.getRooms(), flat.isBalcony(), flat.getArea());
         return create;
     }
 
     @Override
     public int update(Flat flat) {
-        String sql = "UPDATE flats SET rooms = ?, balcony = ?, area = ? WHERE fl_no = ?";
+        String sql = "UPDATE Flats SET rooms = ?, balcony = ?, area = ? WHERE fl_no = ?";
         int update = jdbcTemplate.update(sql, flat.getRooms(), flat.isBalcony(), flat.getArea(), flat.getFl_no());
         return update;
     }
 
     @Override
     public int delete(String fl_no) {
-        String sql = "DELETE FROM flats WHERE fl_no = ?";
+        String sql = "DELETE FROM Flats WHERE fl_no = ?";
         jdbcTemplate.update(sql, fl_no);
         return 0;
     }
 
     @Override
     public List<Flat> read() {
-        String sql = "SELECT * FROM flats";
+        String sql = "SELECT * FROM Flats";
         BeanPropertyRowMapper<Flat> rowMapper = BeanPropertyRowMapper.newInstance(Flat.class);
         List<Flat> result = jdbcTemplate.query(sql, rowMapper);
         return result;
