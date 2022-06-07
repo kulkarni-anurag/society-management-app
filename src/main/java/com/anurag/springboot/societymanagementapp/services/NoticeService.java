27433b8a35b640c6e19116fd8429662d82b52088
Anurag Kulkarni <kanurag0801@gmail.com>
2022-06-07 21:51:06 +0530

    Added notices service

---
+++ src/main/java/com/anurag/springboot/societymanagementapp/services/NoticeService.java
@@ -0,0 +1,36 @@
+package com.anurag.springboot.societymanagementapp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anurag.springboot.societymanagementapp.DAO.NoticeDAO;
+import com.anurag.springboot.societymanagementapp.model.Notice;
+
+@Service
+public class NoticeService {
+
+    @Autowired
+    private NoticeDAO noticeDAO;
+
+    public List<Notice> getAllNotices(){
+        return noticeDAO.read();
+    }
+
+    public Notice getOneNotice(int notice_id){
+        return noticeDAO.read(notice_id);
+    }
+
+    public void addNotice(Notice notice){
+        noticeDAO.create(notice);
+    }
+
+    public void updateNotice(Notice notice){
+        noticeDAO.update(notice);
+    }
+
+    public void deleteNotice(int notice_id){
+        noticeDAO.delete(notice_id);
+    }
+}
