53fe475043e30a28a16799f75dc28dbbc31c4c15
Anurag Kulkarni <kanurag0801@gmail.com>
2022-06-08 16:13:17 +0530

    Added basic security config

---
+++ src/main/java/com/anurag/springboot/societymanagementapp/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.anurag.springboot.societymanagementapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService user(){
+        UserDetails user = User.builder()
+            .username("admin")
+            .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
